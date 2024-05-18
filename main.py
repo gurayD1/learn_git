7c2ae4548b6e4d8e745cfa474444c77e0e5ea325
gurayD1 <83139326+gurayD1@users.noreply.github.com>
2024-05-18 16:20:48 -0400

    test1

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
 def greet():
-    print("Hello from the main branch!")
-    print("Hello from the feature branch!")
-    print(" main Hello from the feature branch!")
+    print("Hello")
+    print("Hello from the feature main!")
